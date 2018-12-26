e69126693c98920fb232abe746a6cfaa9a10165e
Maximo Bautista <max.bautista97@gmail.com>
2018-12-26 18:07:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,10 +64,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
-programmer_hash = [:yukihiro_matsumoto][:known_for] = "Ruby"
-programmer_hash = [:yukihiro_matsumoto][:languages] = ["LISP", "C"]
-         
+  
 	programmer_hash = 
  		{ 
         :grace_hopper => {
@@ -84,6 +81,8 @@ programmer_hash = [:yukihiro_matsumoto][:languages] = ["LISP", "C"]
         }
      }
 
+programmer_hash = [:yukihiro_matsumoto][:known_for] = "Ruby"
+programmer_hash = [:yukihiro_matsumoto][:languages] = ["LISP", "C"]
     
 end
 
