63756437fed60a5140c7ed3824a4f8eb48d36a0c
Maximo Bautista <max.bautista97@gmail.com>
2018-12-26 18:09:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,9 +81,9 @@ def adding_matz
         }
      }
 
-programmer_hash = [:yukihiro_matsumoto][:known_for] = "Ruby"
-programmer_hash = [:yukihiro_matsumoto][:languages] = ["LISP", "C"]
-programmer_hash
+  programmer_hash = [:yukihiro_matsumoto][:known_for] = "Ruby"
+  programmer_hash = [:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+  programmer_hash
 end
 
 def changing_alan
@@ -103,7 +103,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
      
      
 end
